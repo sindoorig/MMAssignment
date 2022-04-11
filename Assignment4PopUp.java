71ba9b3bdd931f06b501bbbd3c7aeeaf60513ed5
sindoorig <gsindoori@gmail.com>
2022-04-11 16:24:04 -0400

    Update Assignment4PopUp.java

--- Assignment4PopUp.java
+++ Assignment4PopUp.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Dialogue {
+public class Assignment4PopUp {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");  
